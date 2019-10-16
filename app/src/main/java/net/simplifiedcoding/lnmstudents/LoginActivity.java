9581c82984cc479e938269f5f5eff9d936901301
yashnahta <yashnahtanaman12345@gmail.com>
2019-10-16 12:00:45 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/lnmstudents/LoginActivity.java
@@ -0,0 +1,33 @@
+package net.simplifiedcoding.lnmstudents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+
+import net.simplifiedcoding.navigationdrawerexample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = (TextView)findViewById(R.id.lnkRegister);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void login(View view){
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/net/simplifiedcoding/lnmstudents/RegistrationActivity.java
@@ -0,0 +1,32 @@
+package net.simplifiedcoding.lnmstudents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import net.simplifiedcoding.navigationdrawerexample.R;
+
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        TextView login = (TextView)findViewById(R.id.lnkLogin);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void login(View view){
+        Intent intent=new Intent(RegistrationActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
